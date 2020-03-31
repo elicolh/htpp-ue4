f12c1631518168e8a082a29b4b683cee8569915c
elicolh <36312455+elicolh@users.noreply.github.com>
2020-03-31 13:04:18 +0200

    init

---
+++ main.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql")
+const express = require("express")
+const bodyParser = require("body-parser")
+const serv = express()
+    serv.use(express.static("public"))
+    serv.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended: true}))
+
+app.post("localhost:5678",(req,res)=>{
+    connection.query(`SELECT idaccount FROM account WHERE username = '${pseudo}' AND password = '${password}'` , 
+    function (error, results, fields) {
+        if (error) throw error;
+        // res.json({ok:})
+    });
+    connection.end();
+})
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'authenconnect',
+    password : 'elicolelo',
+    database : 'account'
+});
