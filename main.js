1a39617f9bbc9e240819209526c2b9e4c4650120
elicolh <36312455+elicolh@users.noreply.github.com>
2020-04-13 15:58:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -25,23 +25,49 @@ serv.post("/",(req,res)=>{//si on a une requete de type POST au sous domaine "/"
     connection.connect()//init de la connection a la base sql
     connection.query(`SELECT idaccount FROM account WHERE username = '${req.body.pseudo}' AND password = '${req.body.password}'`,//l'objet req.body contient les arguments de la requete (pseudo password et deviceid)
     function (error, results, fields) {//reponse de la query
-        if (error) throw error;//si reponse mal passée on arrete le programme et on print l'erreur
-        connection.end((err)=>{//une fois la connection fermée:
-            if (err) throw err;//si la fermuture s'est mal pasée on arrete le programme et on print l'erreur
-            if(results[0].idaccount){//si la query a retourné un id : le mdp est correct
-                res.json({ID:1})//on repond a la requete
-                connection.connect()//on ouvre une nvle connection
-                connection.query(`UPDATE account SET deviceid = '${req.body.deviceid}' WHERE idaccount = '${results[0].idaccount}'`,
-                function(err,results,fields){//réponse de la 2e query
-                    if(err) throw err//si reponse mal passée on arrete le programme et on print l'erreur
-                })
-                connection.end()//on ferme la nvle connection
-            }else{//si la query a rien retourné : le mdp est faux
-                res.json({ID:0})//on reponds a la requete
-            }
-        });
+    if (error) throw error;//si reponse mal passée on arrete le programme et on print l'erreur
+        if(results[0].idaccount){//si la query a retourné un id : le mdp est correct
+            res.json({ID:1})//on repond a la requete
+            connection.query(`UPDATE account SET deviceid = '${req.body.deviceid}' WHERE idaccount = '${results[0].idaccount}'`,
+            function(err,results,fields){//réponse de la 2e query
+                if(err) throw err//si reponse mal passée on arrete le programme et on print l'erreur
+            })
+            connection.end()//on ferme la nvle connection
+        }else{//si la query a rien retourné : le mdp est faux
+            res.json({ID:0})//on reponds a la requete
+        }
     });
 })
 serv.listen(5678, function () {//on lance l'écoute du serv
     console.log('listening on port 5678')
 })
+
+
+
+// serv.post("/",(req,res)=>{//si on a une requete de type POST au sous domaine "/" 
+//     var connection = mysql.createConnection({//on crée une nvl connection a la DB
+//         host     : 'localhost',
+//         user     : 'authenconnect',
+//         password : 'elicolelo',
+//         database : 'account'
+//     });
+//     connection.connect()//init de la connection a la base sql
+//     connection.query(`SELECT idaccount FROM account WHERE username = '${req.body.pseudo}' AND password = '${req.body.password}'`,//l'objet req.body contient les arguments de la requete (pseudo password et deviceid)
+//     function (error, results, fields) {//reponse de la query
+//         if (error) throw error;//si reponse mal passée on arrete le programme et on print l'erreur
+//         connection.end((err)=>{//une fois la connection fermée:
+//             if (err) throw err;//si la fermuture s'est mal pasée on arrete le programme et on print l'erreur
+//             if(results[0].idaccount){//si la query a retourné un id : le mdp est correct
+//                 res.json({ID:1})//on repond a la requete
+//                 connection.connect()//on ouvre une nvle connection
+//                 connection.query(`UPDATE account SET deviceid = '${req.body.deviceid}' WHERE idaccount = '${results[0].idaccount}'`,
+//                 function(err,results,fields){//réponse de la 2e query
+//                     if(err) throw err//si reponse mal passée on arrete le programme et on print l'erreur
+//                 })
+//                 connection.end()//on ferme la nvle connection
+//             }else{//si la query a rien retourné : le mdp est faux
+//                 res.json({ID:0})//on reponds a la requete
+//             }
+//         });
+//     });
+// })
