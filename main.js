52ffa117c50bfae5de95eb06f031526137ca8924
elicolh <36312455+elicolh@users.noreply.github.com>
2020-04-13 15:30:42 +0200

    UPDATE account SET deviceid

--- main.js
+++ main.js
@@ -6,40 +6,42 @@ const serv = express()
     serv.use(bodyParser.json())
     serv.use(bodyParser.urlencoded({extended: true}))
 
-// serv.post("localhost:5678",(req,res)=>{
-//     connection.query(`SELECT idaccount FROM account WHERE username = '${pseudo}' AND password = '${password}'` , 
-//     function (error, results, fields) {
-//         if (error) throw error;
-//         // res.json({ok:})
-//     });
-//     connection.end();
-// })
-// "SELECT idaccount FROM account WHERE username = 'server' AND password = 'a66a8e9b1cf6d2032724c37305b83cdda18c904e';"
-serv.post("/",(req,res)=>{
-    var connection = mysql.createConnection({
+/*serv.post("localhost:5678",(req,res)=>{
+    connection.query(`SELECT idaccount FROM account WHERE username = '${pseudo}' AND password = '${password}'` , 
+    function (error, results, fields) {
+        if (error) throw error;
+        // res.json({ok:})
+    });
+    connection.end();
+})
+"SELECT idaccount FROM account WHERE username = 'server' AND password = 'a66a8e9b1cf6d2032724c37305b83cdda18c904e';"*/
+serv.post("/",(req,res)=>{//si on a une requete de type POST au sous domaine "/" 
+    var connection = mysql.createConnection({//on crée une nvl connection a la DB
         host     : 'localhost',
         user     : 'authenconnect',
         password : 'elicolelo',
         database : 'account'
     });
-    connection.connect()
+    connection.connect()//init de la connection a la base sql
     connection.query(`SELECT idaccount FROM account WHERE username = '${req.body.pseudo}' AND password = '${req.body.password}'`, 
-    function (error, results, fields) {
-        if (error) throw error;
-        try{
-            var nb = results[0].idaccount
-        }catch{
-            var nb = 0
-        }
-        res.json({ID:nb})
-        connection.end();
-        // console.log("pseudo : " + req.body.pseudo)
-        // console.log('mdp : ' + req.body.password)
-        // console.log(`SELECT idaccount FROM account WHERE username = '${req.body.pseudo}' AND password = '${req.body.password}'`)
-        // console.log(results)
-        // console.log(JSON.stringify({ID:nb}))
+    function (error, results, fields) {//reponse de la query
+        if (error) throw error;//si reponse mal passée on arrete le programme et on print l'erreur
+        connection.end((err)=>{//une fois la connection fermée:
+            if (err) throw err;//si la fermuture s'est mal pasée on arrete le programme et on print l'erreur
+            if(results[0].idaccount){//si la query a retourné un id : le mdp est correct
+                res.json({ID:1})//on repond a la requete
+                connection.connect()//on ouvre une nvle connection
+                connection.query(`UPDATE account SET deviceid = '${req.body.deviceid}' WHERE idaccount = '${results[0].idaccount}'`,
+                function(err,results,fields){//réponse de la 2e query
+                    if(err) throw err//si reponse mal passée on arrete le programme et on print l'erreur
+                })
+                connection.end()//on ferme la nvle connection
+            }else{//si la query a rien retourné : le mdp est faux
+                res.json({ID:0})//on reponds a la requete
+            }
+        });
     });
 })
-serv.listen(5678, function () {
+serv.listen(5678, function () {//on lance l'écoute du serv
     console.log('listening on port 5678')
-  })
+})
