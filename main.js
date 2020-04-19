694e306fb9863f3ec6f72fb1cb7e52a502b5f841
Sweikill <36311648+Sweikill@users.noreply.github.com>
2020-04-19 19:05:52 +0200

    up

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');//:flag_dz: :flag_dz: :flag_dz: :flag_dz: :flag_dz:
 const mysql = require("mysql")      
 const express = require("express")
 const bodyParser = require("body-parser")
@@ -19,55 +20,58 @@ serv.post("/",async function(request,response){
     console.log("nvle requete")
     // connection.connect()//init de la connection a la base sql
     connection = await getPoolConnection()
-    var result = await newQuery(`SELECT idaccount FROM account WHERE username = '${request.body.pseudo}' AND password = '${request.body.password}'`)
-    try{var idaccount = result.idaccount}
-    catch{
+    var result = await newQuery(`SELECT password FROM account WHERE name = '${request.body.pseudo}';`)
+    if (!(await bcrypt.compare(request.body.password ,result)))
+        {
         console.log("---------------mauvaise connection---------------".red)
         console.log(request.body)
         response.json({port:0})
-        return
-    }
-    await newQuery(`UPDATE account SET deviceid = '${request.body.Deviceid}' WHERE idaccount = '${idaccount}'`)
-    result = await newQuery(`SELECT idTeam FROM account WHERE idaccount = '${idaccount}'`)
-    try{var idTeam = result.idTeam}
-    catch{
-        console.log("PAS DANS UNE TEAM".red)
-        response.json({port:-1})
-        return
-    }
-    console.log(`idTeam renvoyé :${idTeam}`)
-    result = await newQuery(`SELECT sessionport FROM team WHERE idTeam = '${idTeam}'`)
-    try{
-        var sessionport = result.sessionport
-    }
-    catch{
-        var sessionport = null
-    }
-    if(sessionport){
-        response.json({ port: sessionport })
-        console.log(`port bien renvoyé en json : sessionport = ${sessionport}`)
-    }else{
-        result = await newQuery("SELECT port FROM port WHERE idTeam IS NULL LIMIT 1;")
-        try{
-            var port = result.port
-        }catch{
-            console.log("la query a pas retourné de port libre".red)
-            response.json({port:-2})
+        }
+    else
+        {
+        var id = await newQuery(`SELECT id FROM account WHERE name = '${request.body.pseudo}'`)
+        await newQuery(`UPDATE account SET deviceid = '${request.body.Deviceid}' WHERE id = '${id}'`)
+        result = await newQuery(`SELECT idTeam FROM account WHERE id = '${id}'`)
+        try{var idTeam = result.idTeam}
+        catch{
+            console.log("PAS DANS UNE TEAM".red)
+            response.json({port:-1})
             return
         }
-        console.log(`lancement du .exe, port renvoyé = ${port}`)
-        exec(__dirname + "/startSession.bat", [port])
-        response.json({port:port})
-        await newQuery(`UPDATE team SET sessionport = ${port} WHERE idTeam = ${idTeam};`)
-        await newQuery(`UPDATE port SET idTeam = ${idTeam} WHERE port = ${port};`)
-    }
-    connection.destroy()
+        console.log(`idTeam renvoyé :${idTeam}`)
+        result = await newQuery(`SELECT sessionport FROM team WHERE idTeam = '${idTeam}'`)
+        try{
+            var sessionport = result.sessionport
+        }
+        catch{
+            var sessionport = null
+        }
+        if(sessionport){
+            response.json({ port: sessionport })
+            console.log(`port bien renvoyé en json : sessionport = ${sessionport}`)
+        }else{
+            result = await newQuery("SELECT port FROM port WHERE idTeam IS NULL LIMIT 1;")
+            try{
+                var port = result.port
+            }catch{
+                console.log("la query a pas retourné de port libre".red)
+                response.json({port:-2})
+                return
+            }
+            console.log(`lancement du .exe, port renvoyé = ${port}`)
+            exec(__dirname + "/startSession.bat", [port])
+            response.json({port:port})
+            await newQuery(`UPDATE team SET sessionport = ${port} WHERE idTeam = ${idTeam};`)
+            await newQuery(`UPDATE port SET idTeam = ${idTeam} WHERE port = ${port};`)
+        }
+        connection.destroy()
+        }
 })
 
 serv.listen(6999, function () {//on lance l'écoute du serv
     console.log('listening on port 6999')
 })
-function newQuery(query){
+async function newQuery(query){
     return new Promise((resolve,reject)=>{
         connection.query(query,(error,results,field)=>{
             if(error) throw error
